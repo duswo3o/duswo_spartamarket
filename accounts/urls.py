66aee13fb2b20264ace47ddfeb144e894444195a
yeonjae <pyj3740@gmail.com>
2024-08-20 01:20:26 +0900

    feat : 로그인
    로그인 기능 구현
    현재는 home으로 설정할만한 페이지가 없어 임세로 페이지를 만듦
    나중에 testhome html 삭제 필요

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("signup/", views.signup, name="signup"),
+    path("", views.testhome, name="test"),
+    path("login/", views.login, name="login"),
+    # path("logout/", views.logout, name="logout")
+    # path("signup/", views.signup, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST, require_http_methods
 
+from django.contrib.auth.forms import AuthenticationForm # 로그인 폼
+from django.contrib.auth import login as auth_login # 로그인
+from django.contrib.auth import logout as auth_logout # 로그아웃
+
+
+from .forms import UserForm
 # Create your views here.
-def signup(request):
-    return render(request, "accounts/signup.html")
+
+
+def testhome(request):
+    return render(request, "accounts/testhome.html")
+
+
+@require_http_methods(["POST", "GET"])
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("accounts:test")
+
+    form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+
+# @require_http_methods(["GET", "POST"])
+# def signup(request):
+#     if request.method == "POST":
+#
+#     form = UserForm()
+#     return render(request, "accounts/signup.html", {"form": form})
